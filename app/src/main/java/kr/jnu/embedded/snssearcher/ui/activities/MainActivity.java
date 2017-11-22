e331f822634731fbc1629bff0b2636cdc9a28dc5
HardPlant <abc7988se@naver.com>
2017-11-22 14:57:33 +0900

    Initial commit

---
+++ app/src/main/java/kr/jnu/embedded/snssearcher/ui/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.jnu.embedded.snssearcher.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.jnu.embedded.snssearcher.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
