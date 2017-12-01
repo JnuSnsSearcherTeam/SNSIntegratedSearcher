837e72e5ba8949b5039c7e2697499171357e9e84
HardPlant <abc7988se@naver.com>
2017-12-01 18:41:55 +0900

    fetcher detach complete

--- app/src/main/java/kr/jnu/embedded/snssearcher/core/FacebookSearcherPresenter.java
+++ app/src/main/java/kr/jnu/embedded/snssearcher/core/FacebookSearcherPresenter.java
@@ -16,8 +16,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.StringReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 /**
  * Created by KANG on 2017-11-22.
@@ -29,12 +32,6 @@ public class FacebookSearcherPresenter implements SNSSearcherContract.Presenter 
     private AccessToken accessToken;
 
     private SNSSearcherContract.View view;
-    private ArrayList<JSONObject> idArray = new ArrayList<>();
-    private ArrayList<String> pageArray = new ArrayList<>();
-    private ArrayList<JSONObject> resultArray = new ArrayList<>();
-
-    private boolean isRequestAllSent;
-    private int requestId = 0;
 
     @Override
     public void setView(SNSSearcherContract.View view) {
@@ -42,8 +39,21 @@ public class FacebookSearcherPresenter implements SNSSearcherContract.Presenter 
     }
 
     @Override
-    public void loadItem(SNSSearcherContract.LoadCompleteListner listener) {
-        startGetPageCandidates(listener);
+    public void loadItem(final SNSSearcherContract.LoadCompleteListner listener) {
+        final ArrayList<JSONObject> result = new ArrayList<>();
+
+        FacebookPagePostFetcher facebookPagePostFetcher
+                = new FacebookPagePostFetcher(accessToken, result);
+
+        facebookPagePostFetcher.onResult(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                listener.onComplete(result);
+                return true;
+            }
+        });
+
+        facebookPagePostFetcher.start();
     }
 
     public FacebookSearcherPresenter() {
@@ -62,199 +72,4 @@ public class FacebookSearcherPresenter implements SNSSearcherContract.Presenter 
     }
 
 
-    private void startGetPageCandidates(final SNSSearcherContract.LoadCompleteListner listener){
-        Log.d(TAG,"Access Token : " + accessToken);
-
-        GraphRequest friendRequest = GraphRequest.newMeRequest(
-                accessToken,
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(
-                            JSONObject object,
-                            GraphResponse response) {
-                        if(response.getError() != null) Log.d(TAG, response.getError().toString());
-                        if(object == null) return;
-
-                        Log.d(TAG, "Friend list:" + object.toString());
-                        addFacebookId(object);
-                    }
-                });
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "friends");
-
-        GraphRequestBatch batch = new GraphRequestBatch(friendRequest);
-        batch.addCallback(new GraphRequestBatch.Callback() {
-            @Override
-            public void onBatchCompleted(GraphRequestBatch batch) {
-                Log.d(TAG, "Facebook IDs: " + getFacebookIdArray());
-                getPageIdFromLikes(listener);
-            }
-        });
-        batch.executeAsync();
-    }
-
-
-    private void getPageIdFromLikes(final SNSSearcherContract.LoadCompleteListner listener){
-        ArrayList<GraphRequest> requests = new ArrayList<>();
-        GraphRequest userRequest = GraphRequest.newMeRequest(
-                accessToken,
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        if(response.getError() != null) Log.d(TAG, response.getError().toString());
-                        if(object == null) return;
-                        Log.d(TAG,"newMeRequest Completed.");
-                        try {
-                            addPage(object.getJSONObject("likes").getJSONArray("data"));
-                        } catch (JSONException e){
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        );
-        Bundle likeParameters = new Bundle();
-        likeParameters.putString("fields", "likes{id}");
-        userRequest.setParameters(likeParameters);
-        requests.add(userRequest);
-
-        try {
-            for (JSONObject data : getFacebookIdArray()) {
-                String id = data.getString("id");
-                requests.add(
-                        GraphRequest.newGraphPathRequest(
-                                accessToken,
-                                id + "/likes"
-                                , new GraphRequest.Callback() {
-                                    @Override
-                                    public void onCompleted(GraphResponse response) {
-                                        if(response.getError() != null) Log.d(TAG, "id likes error:" + response.getError());
-                                        addPage(response.getJSONObject());
-                                    }
-                                }
-                        ));
-            }
-        }catch(JSONException e){
-            //e.printStackTrace();
-        }
-        GraphRequestBatch batch = new GraphRequestBatch(requests);
-        batch.addCallback(new GraphRequestBatch.Callback() {
-            @Override
-            public void onBatchCompleted(GraphRequestBatch batch) {
-                Log.d(TAG, "Page IDs: " + getPageArray());
-                getFeedsFromPageArray(listener);
-            }
-        });
-        batch.executeAsync();
-    }
-    private void getFeedsFromPageArray(final SNSSearcherContract.LoadCompleteListner listener){
-        StringBuffer pages = new StringBuffer();
-        int count = 0;
-
-        for(String id : pageArray){
-            pages.append(id).append(',');
-            count++;
-            if(pageArray.isEmpty() || count > 5){
-                pages.deleteCharAt(pages.length() - 1);
-                requestId++;
-                sendPageFeedRequest(requestId, pages.toString(), listener);
-                pages.delete(0, pages.length());
-                count = 0;
-            }
-        }
-        isRequestAllSent = true;
-    }
-
-    public int getRequestId() {
-        return requestId;
-    }
-
-    public boolean isRequestAllSent() {
-        return isRequestAllSent;
-    }
-
-    private void sendPageFeedRequest(final int requestId, String pids, final SNSSearcherContract.LoadCompleteListner listener){
-        GraphRequest request = GraphRequest.newGraphPathRequest(
-                accessToken,
-                "feed?limit=5&ids=" + pids
-                , new GraphRequest.Callback() {
-                    @Override
-                    public void onCompleted(GraphResponse response) {
-                        if(response.getError() != null) Log.d(TAG, "id likes error:" + response.getError());
-                        addPage(response.getJSONObject());
-                        Log.d(TAG, "Pages : " + response.getJSONObject());
-                        if(isRequestAllSent() && requestId == getRequestId()){
-                            Log.d(TAG,"All request Completed.");
-                        }
-                    }
-                }
-        );
-        Log.d(TAG,"Graph Path :"+ request.getGraphPath());
-        Log.d(TAG,"HTTP Method :"+ request.getHttpMethod());
-
-        request.executeAsync();
-    }
-
-    public void getMe(final SNSSearcherContract.LoadCompleteListner listener) {
-        GraphRequest request = GraphRequest.newMeRequest(
-                accessToken,
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(
-                            JSONObject object,
-                            GraphResponse response) {
-                        if(response.getError() != null) Log.d(TAG, response.getError().toString());
-                        if(object == null) return;
-
-                        Log.d(TAG, "GetMe Result:" + object.toString());
-                        addFacebookId(object);
-                        listener.onComplete(idArray);
-                    }
-                });
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "id,name,link");
-        request.setParameters(parameters);
-        request.executeAsync();
-    }
-
-    public void addFacebookId(JSONObject object){
-        this.idArray.add(object);
-    }
-
-    public void addFacebookId(JSONArray objects){
-        try {
-            for(int i=0; i < objects.length(); i++) {
-                this.idArray.add(objects.getJSONObject(i));
-            }
-        }
-        catch(JSONException e){
-            e.printStackTrace();
-        }
-    }
-
-    public ArrayList<JSONObject> getFacebookIdArray(){
-        return this.idArray;
-    }
-
-    public ArrayList<String> getPageArray() {
-        return pageArray;
-    }
-
-    public void addPage(JSONObject object) {
-        try {
-            this.pageArray.add(object.getString("id"));
-        } catch (JSONException e){
-
-        }
-    }
-
-    public void addPage(JSONArray objects) {
-        try {
-            for(int i=0; i < objects.length(); i++) {
-                addPage(objects.getJSONObject(i));
-            }
-        }
-        catch(JSONException e){
-            e.printStackTrace();
-        }
-    }
 }
