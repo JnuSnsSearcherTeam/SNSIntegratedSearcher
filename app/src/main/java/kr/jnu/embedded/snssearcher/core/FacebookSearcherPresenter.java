af1d3e0420614f4c8d815da97bd0bd5238b40115
HardPlant <abc7988se@naver.com>
2017-12-01 19:38:41 +0900

    parse facebook page first

--- app/src/main/java/kr/jnu/embedded/snssearcher/core/FacebookSearcherPresenter.java
+++ app/src/main/java/kr/jnu/embedded/snssearcher/core/FacebookSearcherPresenter.java
@@ -1,11 +1,25 @@
 package kr.jnu.embedded.snssearcher.core;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Picture;
+import android.media.Image;
+
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
+import kr.jnu.embedded.snssearcher.data.FacebookPage;
 import kr.jnu.embedded.snssearcher.data.FacebookPagePost;
 
 /**
@@ -41,6 +55,32 @@ public class FacebookSearcherPresenter implements SNSSearcherContract.Presenter 
 
         facebookPagePostFetcher.start();
     }
+
+    public void parsePages(JSONObject pageInfo, JSONObject fetchedPageResult){
+        try {
+            ArrayList<FacebookPage> pages;
+            ArrayList<FacebookPagePost> posts;
+
+            String PageID;
+            JSONArray pageInfoData = pageInfo.getJSONArray("data");
+            for(int i=0; i<pageInfoData.length();i++){
+                JSONObject info = pageInfoData.getJSONObject(i);
+                String name = info.getString("name");
+                String picture = info.getJSONObject("pricture").getJSONObject("data").getString("url");
+
+                FacebookPage page = new FacebookPage(picture, name);
+            }
+
+            Image icon;
+            String name;
+            String message;
+
+        } catch(JSONException je){
+            je.printStackTrace();
+        }
+
+    }
+
 
     public FacebookSearcherPresenter() {
         CallbackManager mCallbackManager;
