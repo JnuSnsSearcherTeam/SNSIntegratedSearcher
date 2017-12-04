3468ac6953bb35b5ca81a6a3363491812ed627bc
HardPlant <abc7988se@naver.com>
2017-12-04 18:50:21 +0900

    login token works well.

--- app/src/main/java/kr/jnu/embedded/snssearcher/ui/activities/FacebookLoginActivity.java
+++ app/src/main/java/kr/jnu/embedded/snssearcher/ui/activities/FacebookLoginActivity.java
@@ -1,14 +1,21 @@
 package kr.jnu.embedded.snssearcher.ui.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenManager;
+import com.facebook.AccessTokenTracker;
 import com.facebook.login.LoginManager;
 import java.util.Arrays;
 
 import kr.jnu.embedded.snssearcher.R;
+import kr.jnu.embedded.snssearcher.base.App;
 import kr.jnu.embedded.snssearcher.core.InstagramLoginManager;
 
 public class FacebookLoginActivity extends AppCompatActivity {
@@ -18,6 +25,8 @@ public class FacebookLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_facebook_login);
         Button facebookButton = findViewById(R.id.facebookButton);
         Button instagramButton = findViewById(R.id.instagramButton);
+        Button checkButton = findViewById(R.id.checkAccessToken);
+
 
         facebookButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +38,13 @@ public class FacebookLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 instaLogin();
+            }
+        });
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Activity","Instagram: " + ((App)getApplicationContext()).getInstagramAccessToken());
+                Log.d("Activity","facebook" + AccessToken.getCurrentAccessToken());
             }
         });
 
